3523fdc1479832de1d42a7aad12eb80350a3f66a
Albert Podraza <albert.podraza@ge.com>
2018-04-14 16:28:53 +0200

    balance temp fix

--- src/main/java/com/dwe/dealwitheat/transaction/db/TransactionDao.java
+++ src/main/java/com/dwe/dealwitheat/transaction/db/TransactionDao.java
@@ -19,40 +19,64 @@ public class TransactionDao {
     private JdbcTemplate jdbcTemplate;
 
     public int countAll(String email) {
-        String query = "SELECT sum(t.count) FROM transaction t " +
-                "            LEFT JOIN offer o ON (o.id=t.offer_id) " +
-                "            LEFT JOIN restaurant r ON (r.id=o.restaurant_id) " +
-                "            LEFT JOIN restaurant_employee e ON (r.id=e.restaurant_id) " +
-                "WHERE e.email=\'" + email + "\'";
+        String query = "SELECT count(*) FROM\n" +
+                "(\n" +
+                "SELECT \n" +
+                "t.id\n" +
+                "FROM transaction t   \n" +
+                "LEFT JOIN transaction_offer_link tol ON (tol.transaction_id=t.id)\n" +
+                "LEFT JOIN offer o ON (o.id=tol.offer_id)             \n" +
+                "LEFT JOIN restaurant r ON (r.id=o.restaurant_id)             \n" +
+                "LEFT JOIN restaurant_employee e ON (r.id=e.restaurant_id) \n" +
+                "WHERE e.email=\'" + email + "\' " +
+                "GROUP BY t.id\n" +
+                ") AS temp";
         return jdbcTemplate.queryForObject(query, Integer.class);
     }
 
     public int countByState(String email, String state) {
-        String query = "SELECT sum(t.count) FROM transaction t " +
-                "LEFT JOIN offer o ON (o.id=t.offer_id) " +
-                "LEFT JOIN restaurant r  ON (r.id=o.restaurant_id) " +
-                "LEFT JOIN restaurant_employee e ON (r.id=e.restaurant_id) " +
-                "WHERE e.email=\'" + email + "\' AND t.state=\'" + state + "\'";
+        String query = "SELECT count(*) FROM\n" +
+                "(\n" +
+                "SELECT t.id FROM transaction t \n" +
+                "LEFT JOIN transaction_offer_link tol ON (tol.transaction_id=t.id)\n" +
+                "LEFT JOIN offer o ON (o.id=tol.offer_id) \n" +
+                "LEFT JOIN restaurant r  ON (r.id=o.restaurant_id) \n" +
+                "LEFT JOIN restaurant_employee e ON (r.id=e.restaurant_id) \n" +
+                "WHERE e.email=\'" + email + "\' AND t.state=\'" + state + "\' " +
+                "GROUP BY t.id\n" +
+                ") AS temp;";
         return jdbcTemplate.queryForObject(query, Integer.class);
     }
 
+    public StringBuilder in(List<String> zipCodeList) {
+        StringBuilder zipCodes = new StringBuilder();
+        zipCodes.append("(");
+        for (int i = 0; i < zipCodeList.size() - 1; i++) {
+            zipCodes.append( zipCodeList.get(i)+ ",");
+        }
+        zipCodes.append( zipCodeList.get(zipCodeList.size() - 1) + ")");
+        return zipCodes;
+    }
 
     public double countTakings(String email, long type) {
+        String transactionIdsQuery = "SELECT distinct toc.transaction_id FROM offer o\n" +
+                "\t\t\tLEFT JOIN transaction_offer_link toc ON (toc.offer_id=o.id)\n" +
+                "            LEFT JOIN restaurant r ON (r.id=o.restaurant_id)\n" +
+                "            LEFT JOIN restaurant_employee e ON (r.id=e.restaurant_id)\n" +
+                "WHERE email = '" + email + "';";
+        List <String>transactionsIds = jdbcTemplate.query(transactionIdsQuery, ((rs, rowNum) -> {
+            return Integer.toString(rs.getInt(1));
+        }));
         String query;
         if (type == 0) {
-            query = "SELECT sum(o.price) FROM offer o\n" +
-                    "LEFT JOIN transaction t ON (t.offer_id=o.id)\n" +
-                    "            LEFT JOIN restaurant r ON (r.id=o.restaurant_id)\n" +
-                    "            LEFT JOIN restaurant_employee e ON (r.id=e.restaurant_id)\n" +
-                    "WHERE email = '" + email + "';";
+            query = "SELECT sum(o.price) FROM offer o where id in " + in(transactionsIds).toString() + " ";
         } else {
             java.sql.Date date = new java.sql.Date(Calendar.getInstance().getTime().getTime() - type);
-            query = "SELECT sum(o.price) FROM offer o\n" +
-                    "LEFT JOIN transaction t ON (t.offer_id=o.id)\n" +
-                    "            LEFT JOIN restaurant r ON (r.id=o.restaurant_id)\n" +
-                    "            LEFT JOIN restaurant_employee e ON (r.id=e.restaurant_id)\n" +
-                    "WHERE email = \'" + email + "\' " +
-                    "AND date > \'" + date.toString() + "\'";
+            query = "SELECT sum(o.price) FROM offer o " +
+                    "left join transaction_offer_link toc on (toc.offer_id=o.id)  " +
+                    "left join transaction t on (toc.transaction_id=t.id) " +
+                    "where t.id in " + in(transactionsIds).toString() +  " " +
+                    "AND t.order_time > \'" + date.toString() + "\'";
         }
         Double result = jdbcTemplate.queryForObject(query, Double.class);
         return result == null ? 0 : result;
