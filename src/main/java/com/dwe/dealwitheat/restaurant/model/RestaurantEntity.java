e46d16ec8019bb03b2186f70f5a5cce3d5215184
Albert Podraza <albert.podraza@ge.com>
2018-06-02 13:39:32 +0200

    Coordinate unification

--- src/main/java/com/dwe/dealwitheat/restaurant/model/RestaurantEntity.java
+++ src/main/java/com/dwe/dealwitheat/restaurant/model/RestaurantEntity.java
@@ -1,5 +1,7 @@
 package com.dwe.dealwitheat.restaurant.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import javax.persistence.*;
 
 @Entity
@@ -13,7 +15,9 @@ public class RestaurantEntity {
     private long id;
     private String name;
     private String address;
+    @JsonProperty("lat")
     private double latitude;
+    @JsonProperty("lng")
     private double longtitude;
     private String description;
     private String website;
