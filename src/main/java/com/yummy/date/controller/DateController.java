3cbe09f095dd1ef862e9a7bd1aaee4a921f0c4dd
Albert Podraza <albert.podraza@ge.com>
2018-11-04 17:25:06 +0100

    Origin header fix

--- src/main/java/com/yummy/date/controller/DateController.java
+++ src/main/java/com/yummy/date/controller/DateController.java
@@ -1,11 +1,6 @@
 package com.yummy.date.controller;
 
-import com.yummy.date.model.DateResponse;
-import com.yummy.date.service.DateService;
-import com.yummy.date.service.DateService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yummy.commons.Response;
 import com.yummy.date.service.DateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,24 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DateController {
 
-
-    private ObjectMapper mapper;
-
-    public DateController() {
-        mapper = new ObjectMapper();
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    }
-
     @Autowired
     private DateService dateService;
 
+    public DateController(DateService dateService) {
+        this.dateService = dateService;
+    }
+
     @GetMapping(value = "date")
-    private String getDateData() {
-        DateResponse response = dateService.getCurrentDateData();
-        try {
-            return mapper.writeValueAsString(response);
-        } catch (JsonProcessingException e) {
-            return "[]";
-        }
+    private Response getDateData() {
+        return dateService.getCurrentDateData();
     }
 }
